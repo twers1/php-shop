4a16d80c27cb55deff94367b096115ed029471d6
twers1 <tarverdans15@gmail.com>
2024-01-20 21:58:10 +0300

    add script js, style css | edit admin header and panel

--- admin_header.php
+++ admin_header.php
@@ -7,8 +7,35 @@
     <title>Document</title>
 </head>
 <body>
-    <header>
-
+    <header class="header">
+        <div class="flex">
+            <a href="admin_panel.php" class="logo"><img src="img/logo.png" alt="logo"></a>
+            <nav class="navbar">
+                <a href="admin_panel.php">home</a>
+                <a href="admin_product.php">products</a>
+                <a href="admin_order.php">orders</a>
+                <a href="admin_user.php">users</a>
+                <a href="admin_message.php">message</a>
+            </nav>
+            <div class="icons">
+                <i class="bx bxs-user" id="user-btn"></i>
+                <i class="bx bx-list-ul" id="menu-btn"></i>
+            </div>
+            <div class="user-box">
+                <p>username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
+                <p>Email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
+                <form action="" method="post">
+                    <button type="submit" name="logout_btn" class="logout-btn">logout</button>
+                </form>
+            </div>
+        </div>
     </header>
+    <div class="banner">
+        <div class="detail">
+            <h1>admin dashboard</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>
+    <div class="line"></div>
 </body>
 </html>
--- admin_panel.php
+++ admin_panel.php
@@ -17,11 +17,114 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>admin page</title>
 </head>
 <body>
-    <?php include 'admin_header.php';
-
-    ?>
+    <?php include 'admin_header.php';?>
+    <div class="line4"></div>
+    <section class="dashboard">
+        <div class="box-container">
+            <!-- Заказы (в обработке) -->
+            <div class="box">
+                <?php
+                    $total_pendings = 0;
+                    $select_pendings = mysqli_query($conn, "SELECT * FROM `order` WHERE payment_status = 'pending'") or die('query failed');
+                    while($fetch_pending = mysqli_fetch_assoc($select_pendings)){
+                        $total_pendings += $fetch_pending['total_price'];
+                    }
+                ?>
+                <h3>
+                    $ <?php
+                     echo $total_pendings;
+                    ?>/-</h3>
+                <p>total pendings</p>
+                </h3>
+            </div>
+            <!-- Заказы (сделанные) -->
+            <div class="box">
+                <?php
+                    $total_completes = 0;
+                    $select_completes = mysqli_query($conn, "SELECT * FROM `order` WHERE payment_status = 'complete'") or die('query failed');
+                    while($fetch_completes = mysqli_fetch_assoc($select_completes)){
+                        $total_completes += $fetch_completes['total_price'];
+                    }
+                ?>
+                <h3>
+                    $ <?php
+                     echo $total_completes;
+                    ?>/-</h3>
+                <p>total completes</p>
+                </h3>
+            </div>
+            <!-- Заказы  -->
+            <div class="box">
+                <?php
+                    $select_products = mysqli_query($conn, "SELECT * FROM `order`") or die('query failed');
+                    $num_of_products = mysqli_num_rows($select_products);
+                ?>
+                <h3>
+                    $ <?php
+                     echo $num_of_products;
+                    ?>/-</h3>
+                <p>products added</p>
+                </h3>
+            </div>
+            <!-- Пользователи -->
+            <div class="box">
+                <?php
+                    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
+                    $num_of_users = mysqli_num_rows($select_users);
+                ?>
+                <h3>
+                    <?php
+                     echo $num_of_users;
+                    ?></h3>
+                <p>total normal users</p>
+                </h3>
+            </div>
+            <!-- Администраторы -->
+            <div class="box">
+                <?php
+                    $select_admins = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'admin'") or die('query failed');
+                    $num_of_admins = mysqli_num_rows($select_admins);
+                ?>
+                <h3>
+                    <?php
+                     echo $num_of_admins;
+                    ?></h3>
+                <p>total normal admins</p>
+                </h3>
+            </div>
+            <!-- Регистрированные -->
+            <div class="box">
+                <?php
+                    $select_users = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+                    $num_of_users = mysqli_num_rows($select_users);
+                ?>
+                <h3>
+                    <?php
+                     echo $num_of_users;
+                    ?></h3>
+                <p>total registered</p>
+                </h3>
+            </div>
+            <!-- Сообщения -->
+            <div class="box">
+                <?php
+                    $select_message = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+                    $num_of_message = mysqli_num_rows($select_message);
+                ?>
+                <h3>
+                    <?php
+                     echo $num_of_message;
+                    ?></h3>
+                <p>new messages</p>
+                </h3>
+            </div>
+        </div>
+    </section>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
